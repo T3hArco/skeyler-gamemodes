33dcdf3e9de9710724b93ee62df98709ec1d0939
CaptainBigButt <vandocaster@gmail.com>
2013-11-09 19:01:57 -0500

    Added Flame Halo
    Added flame halo (hat) for testing, removed duplicate coordinates (afro)

--- ssbase/gamemode/storeitems/hats/halo_flame.lua
+++ ssbase/gamemode/storeitems/hats/halo_flame.lua
@@ -1,19 +1,19 @@
-ITEM.ID = "afro"											-- Should be a unique string that identifies the item
-ITEM.Name = "Afro"											-- The name the item should display
+ITEM.ID = "halo_flame"										-- Should be a unique string that identifies the item
+ITEM.Name = "Flame Halo"									-- The name the item should display
  
 ITEM.Price = 2000
  
-ITEM.Model = "models/mrgiggles/skeyler/hats/afro.mdl"		-- Model used by the item
+ITEM.Model = "models/mrgiggles/skeyler/hats/halo_flame.mdl"	-- Model used by the item
 
-ITEM.Type = "headcoverfull"									-- Also works for stuff like "mask" and such. Used for item compatibility
+ITEM.Type = "hat"											-- Also works for stuff like "mask" and such. Used for item compatibility
 
 ITEM.Colorable = false										-- Used if the model is colorable via setcolor (or in a models case, setplayercolor)
 ITEM.Tintable = false										-- Used if the model is colorable, but a translation is needed to $selfillumtint 
 
 ITEM.Rotate = 45
 
-ITEM.CamPos = Vector(50, 30, 3)								-- Used the modify the position of the camera on DModelPanels 
-ITEM.LookAt = Vector(0, 0, 3) 								-- Used to change the angle at which the camera views the model 
+ITEM.CamPos = Vector(50, 30, 0)								-- Used the modify the position of the camera on DModelPanels 
+ITEM.LookAt = Vector(0, 0, 0) 								-- Used to change the angle at which the camera views the model 
 ITEM.Fov = 20 
 
 ITEM.Functions = {} 										-- Anything that can be called but not a gmod hook but more of a "store hook" goes here
@@ -34,12 +34,12 @@ ITEM.Bone = "ValveBiped.Bip01_Head1"						-- Bone the item is attached to. ONLY 
 ITEM.BoneMerge = false										-- May be used for certain accessories to bonemerge the item instead. ONLY NEED TO DEFINE FOR HATS/ACCESSORIES.
 
 ITEM.Models = {} 
-ITEM.Models["elin"] = {	["0_0_0_0"]= {pos=Vector(3, 0, 0), ang=Angle(0, -90, -90), scale=1.03}}
-ITEM.Models["miku"] = {	["0_0_0_0"]= {pos=Vector(3, -0.5, 0), ang=Angle(0, -90, -90), scale=0.975}}
-ITEM.Models["tron"] = {	["0_0_0_0"]= {pos=Vector(3.25, 1.0, 0), ang=Angle(0, -90, -90), scale=0.95}}
-ITEM.Models["usif"] = {	["0_0_0_0"]= {pos=Vector(1, 0, 0.2), ang=Angle(0, -90, -90), scale=0.95}}
+ITEM.Models["elin"] = {	["0_0_0_0"]= {pos=Vector(13, -0.5, 0), ang=Angle(0, -90, -90), scale=1}}
+ITEM.Models["miku"] = {	["0_0_0_0"]= {pos=Vector(11, -0.5, 0), ang=Angle(0, -90, -90), scale=1}}
+ITEM.Models["tron"] = {	["0_0_0_0"]= {pos=Vector(13, 0, 0), ang=Angle(0, -90, -90), scale=1}}
+ITEM.Models["usif"] = {	["0_0_0_0"]= {pos=Vector(9.5, 0, 0.2), ang=Angle(0, -90, -90), scale=1}}
 ITEM.Models["zer0"] = {
-	["0_0_0_0"]= {pos=Vector(2.25, 0.25, 0), ang=Angle(0, -90, -90), scale=0.85},
-	["0_1_0_0"]= {pos=Vector(6, 0.25, 0), ang=Angle(0, -90, -90), scale=0.85}
+	["0_0_0_0"]= {pos=Vector(11, 0, 0), ang=Angle(0, -90, -90), scale=1},
+	["0_1_0_0"]= {pos=Vector(6, 0, 0), ang=Angle(0, -90, -90), scale=1}
 }
 /* ************* */
