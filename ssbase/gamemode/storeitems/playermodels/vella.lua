2e7dd88a11a0e139539e35274af5f02493b3834c
CaptainBigButt <vandocaster@gmail.com>
2013-11-21 04:47:20 -0500

    Added bunny ears, updated Vella
    Added bunny bunny ears for testing, added vella playermodel for testing
    (basic rig), updated vella materials

---
+++ ssbase/gamemode/storeitems/playermodels/vella.lua
@@ -0,0 +1,31 @@
+ITEM.ID = "vella"							--Should be a unique string that identifies the item
+ITEM.Name = "Vella"						--The name the item should display
+ 
+ITEM.Price = 200000
+ 
+ITEM.Model = "models/mrgiggles/skeyler/playermodels/vella.mdl"			--Model used by the item
+
+ITEM.Type = "model"								--Also works for stuff like "mask" and such. Used for item compatibility
+
+ITEM.Colorable = false							--Used if the model is colorable via setcolor (or in a models case, setplayercolor)
+ITEM.Tintable = false							--Used if the model is colorable, but a translation is needed to $selfillumtint 
+
+ITEM.CamPos = Vector(50, 30, 64)						-- Used the modify the position of the camera on DModelPanels 
+ITEM.LookAt = Vector(0, 0, 64) 							-- Used to change the angle at which the camera views the model 
+ITEM.Fov = 20 
+
+ITEM.Functions = {} 								--anything that can be called but not a gmod hook but more of a "store hook" goes here
+
+ITEM.Functions["Equip"] = function ()
+												--e.g miku hair attach with the models Equip
+end
+
+ITEM.Functions["Unequip"] = function ()
+												--e.g miku hair attach with the models Equip
+end
+
+ITEM.Hooks = {}
+
+ITEM.Hooks["Think"] = function ()
+												--could run some shit in think hook maybe clientside only (e.g. repositioning or HEALTH CALCULATIONS OR SOMETHING LIKE THAT)
+end
