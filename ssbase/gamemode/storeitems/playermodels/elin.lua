f0d901e3990066db73eb384bf3f321f18deb295e
GeorgeRiley <george_ri@hotmail.co.uk>
2013-11-23 21:36:33 +0000

    whoops

--- ssbase/gamemode/storeitems/playermodels/elin.lua
+++ ssbase/gamemode/storeitems/playermodels/elin.lua
@@ -59,6 +59,7 @@ end
 ITEM.Hooks["PostDrawOpaqueRenderables"] = function (item,ply)
 	if CLIENT && ply.hairtoshow then 
 		if ply == LocalPlayer() and GetViewEntity():GetClass() == 'player' and !LocalPlayer():ShouldDrawLocalPlayer() and !LocalPlayer():GetObserverTarget() then return end		if(ply.currenthair) then
+		if(ply.currenthair) then
 			if(ply.currenthair:GetModel() != ply.hairtoshow) then
 				ply.currenthair:SetModel(ply.hairtoshow)
 			end
