6499a7ecfe02a01b49ca3da4566505ded463e9bd
GeorgeRiley <george_ri@hotmail.co.uk>
2013-11-27 16:45:02 +0000

    Forgot fix for ananas xP

---
+++ bhop/gamemode/mapfixes/bhop_ananas.lua
@@ -0,0 +1,7 @@
+HOOKS["Think"] = function()
+	for k,v in pairs(player.GetAll()) do
+		if(GAMEMODE:IsInArea(v,Vector(5948,-3972,576),Vector(6098,-3900,787))) then
+			v:SetPos(Vector(8429, -5231, 1179))
+		end
+	end
+end
