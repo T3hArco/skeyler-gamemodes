25b78f5afaec3e8ec1accb2bcd8edc55009c554e
GeorgeRiley <george_ri@hotmail.co.uk>
2013-11-28 22:26:04 +0000

    Shoot me, I'm retarded

--- bhop/entities/entities/bhop_area/init.lua
+++ bhop/entities/entities/bhop_area/init.lua
@@ -46,7 +46,7 @@ function ENT:StartTouch(ply)
 		if self:IsSpawn() then 
 			ply.InSpawn = true
 			ply:ResetTimer()  
-		elseif p:IsTimerRunning() then
+		elseif ply:IsTimerRunning() then
 			hook.Call("PlayerWon", GAMEMODE, ply)  
 		end  
 	end 
