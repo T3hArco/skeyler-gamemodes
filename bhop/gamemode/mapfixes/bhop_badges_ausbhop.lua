9debca099c03877f9f57310d84ae07329a9a53c7
GeorgeRiley <george_ri@hotmail.co.uk>
2013-11-28 17:51:30 +0000

    Some map fixes

---
+++ bhop/gamemode/mapfixes/bhop_badges_ausbhop.lua
@@ -0,0 +1,5 @@
+HOOKS["Intialize"] = function()
+	GAMEMODE:AddACArea(Vector(7972, -422, 152),Vector(8667, 484, 352))
+	GAMEMODE:AddACArea(Vector(1913, -3634, 152),Vector(2547, -2411, 352))
+	GAMEMODE:AddACArea(Vector(-4821, -6905, 152),Vector(-4128, -5676, 352))
+end
---
+++ bhop/gamemode/mapfixes/bhop_exodus.lua
@@ -0,0 +1,17 @@
+HOOKS["Intialize"] = function()
+	GAMEMODE:AddACArea(Vector(866, 162, -240),Vector(866, 162, -40),"You have entered the level select area as a result of this your timer has been stopped.")
+	GAMEMODE:AddACArea(Vector(-580, 772, -768),Vector(-315, 894, -568))
+end
+
+HOOKS["InitPostEntity"] = function()
+	for k,v in pairs(ents.FindByClass("trigger_teleport")) do
+		if(v:GetPos() == Vector(6560, 5112, 7412)) then
+			v:SetKeyValue("target","13")
+		end
+	end
+	for k,v in pairs(ents.FindByClass("func_brush")) do
+		if(v:GetName() == "aokilv6") then
+			v:SetName("disabled")
+		end
+	end
+end
