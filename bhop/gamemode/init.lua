2194c441782f534815e5e51c69f9d0427731ac75
GeorgeRiley <george_ri@hotmail.co.uk>
2013-11-29 17:37:55 +0000

    hopeful ljstats

--- bhop/gamemode/init.lua
+++ bhop/gamemode/init.lua
@@ -292,55 +292,57 @@ hook.Add("Think","ACAreas",function()
 	end
 end) --seperate think hooks = more organised and no extra cost in proccessing afaik
 
-hook.Add("Think","StrafeyThink",function()
-	for _,p in pairs(player.GetAll()) do
-		if(!p:IsOnGround()) then
-			if(!p.strafe) then
-				p.strafe = {}
-			end
-			p.curangle = p:GetAngles()
-			if(!p.lastangle) then
-				p.lastangle = p.curangle
-			end
-			if(p.curangle.y < p.lastangle.y) then
-				p.turningleft = false
-			elseif(p.curangle.y > p.lastangle.y) then
-				p.turningleft = true
-			else
-				p.lastangle = p:GetAngles()
-				continue
-			end
-			p.lastangle = p:GetAngles()
+hook.Add("SetupMove","LJStats",function(p,data)
+	if(!p:IsOnGround()) then
+		local dontrun = false
+		if(!p.strafe) then
+			p.strafe = {}
+		end
+		p.curangle = p:EyeAngles()
+		if(p.curangle.y < 0) then
+			p.curangle.y = p.curangle.y + 360
+		end
+		if(!p.lastangle) then
+			p.lastangle = p.curangle.y
+		end
+		if(p.curangle.y == p.lastangle) then
+			dontrun = true
+		end
+		p.lastangle = p:EyeAngles()
 
-			if(p:KeyDown(IN_MOVELEFT) && p.turningleft && (p.strafingright || (!p.strafingright && !p.strafingleft))) then
-				p.strafingright = false
-				p.strafingleft = true
-				p.strafenum = p.strafenum + 1
-				p.strafe[p.strafenum] = {}
-				p.strafe[p.strafenum][1] = 0
-				p.strafe[p.strafenum][2] = 0
-			elseif(p:KeyDown(IN_MOVERIGHT) && !p.turningleft && (p.strafingleft || (!p.strafingright && !p.strafingleft))) then
-				p.strafingright = true
-				p.strafingleft = false
-				p.strafenum = p.strafenum + 1
-				p.strafe[p.strafenum] = {}
-				p.strafe[p.strafenum][1] = 0
-				p.strafe[p.strafenum][2] = 0
-			elseif(!p.strafingleft && !p.strafingright) then
-				continue
-			end
-			local s = p:GetVelocity()
-			s.z = 0
-			p.speed = s:Length()
+		if(p:KeyDown(IN_MOVELEFT) && (p.strafingright || (!p.strafingright && !p.strafingleft))) then
+			p.strafingright = false
+			p.strafingleft = true
+			p.strafenum = p.strafenum + 1
+			p.strafe[p.strafenum] = {}
+			p.strafe[p.strafenum][1] = 0
+			p.strafe[p.strafenum][2] = 0
+		elseif(p:KeyDown(IN_MOVERIGHT) && (p.strafingleft || (!p.strafingright && !p.strafingleft))) then
+			p.strafingright = true
+			p.strafingleft = false
+			p.strafenum = p.strafenum + 1
+			p.strafe[p.strafenum] = {}
+			p.strafe[p.strafenum][1] = 0
+			p.strafe[p.strafenum][2] = 0
+		elseif(p.strafenum == 0) then
+			dontrun = true
+		end
+		if(!p.strafenum) then
+			dontrun = true
+		end
+		if(!dontrun) then
+			p.speed = data:GetVelocity():Length2D()
 			if(p.lastspeed) then
 				local g = p.speed - p.lastspeed
-				if(g > 1) then
+				if(g > 0.5) then
 					p.strafe[p.strafenum][1] = p.strafe[p.strafenum][1] + 1
 				else
 					p.strafe[p.strafenum][2] = p.strafe[p.strafenum][2] + 1
 				end
 			end
 			p.lastspeed = p.speed
+		elseif(p.strafenum && p.strafenum != 0) then
+			p.strafe[p.strafenum][2] = p.strafe[p.strafenum][2] + 1
 		end
 	end
 end)
