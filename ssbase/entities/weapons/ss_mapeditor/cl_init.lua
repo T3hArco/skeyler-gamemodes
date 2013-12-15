8207b6e8622e21781f9d7596dde933b820a7b8af
GeorgeRiley <george_ri@hotmail.co.uk>
2013-12-15 17:53:37 +0000

    Map Editor!

--- ssbase/entities/weapons/ss_mapeditor/cl_init.lua
+++ ssbase/entities/weapons/ss_mapeditor/cl_init.lua
@@ -14,12 +14,11 @@ SWEP.AdminSpawnable		= false
 SWEP.WepSelectIcon		= surface.GetTextureID( "vgui/gmod_tool" ) 
 
 SWEP.First = false 
+SWEP.Second = false 
 
 function SWEP:PrimaryAttack() 
 	if self.NextRun and self.NextRun >= CurTime() then return end 
 	self.NextRun = CurTime()+0.1 
-
-	self.Owner:ChatPrint("ATTACK") 
 	
 	local tr = util.GetPlayerTrace( self.Owner )
 	tr.mask = bit.bor( CONTENTS_SOLID, CONTENTS_MOVEABLE, CONTENTS_MONSTER, CONTENTS_WINDOW, CONTENTS_DEBRIS, CONTENTS_GRATE, CONTENTS_AUX )
@@ -29,9 +28,12 @@ function SWEP:PrimaryAttack()
 	self:ShootEffects(trace.HitPos, trace.HitNormal, trace.Entity, trace.PhysicsBone, IsFirstTimePredicted()) 
 
 	if self.First != false then 
-		self.Owner:ChatPrint("Min = Vector("..math.Round(math.min(self.First.x, trace.HitPos.x))..", "..math.Round(math.min(self.First.y, trace.HitPos.y))..", "..math.Round(math.min(self.First.z, trace.HitPos.z))..")")
-		self.Owner:ChatPrint("Max = Vector("..math.Round(math.max(self.First.x, trace.HitPos.x))..", "..math.Round(math.max(self.First.y, trace.HitPos.y))..", "..math.Round(math.min(self.First.z, trace.HitPos.z))..")") 
+		net.Start("StartPosition")
+		net.WriteVector(self.First)
+		net.WriteVector(trace.HitPos)
+		net.SendToServer()
 		self.First = false  
+		self.Owner:ChatPrint("Start Coordinates Set!")
 	else 
 		self.First = trace.HitPos 
 	end 
@@ -42,13 +44,46 @@ end
    Name: SWEP:SecondaryAttack( )
    Desc: +attack2 has been pressed
 -----------------------------------------------------------]]
-function SWEP:SecondaryAttack()
+function SWEP:SecondaryAttack() 
+	if self.NextRunS and self.NextRunS >= CurTime() then return end 
+	self.NextRunS = CurTime()+0.1 
+	
 	local tr = util.GetPlayerTrace( self.Owner )
 	tr.mask = bit.bor( CONTENTS_SOLID, CONTENTS_MOVEABLE, CONTENTS_MONSTER, CONTENTS_WINDOW, CONTENTS_DEBRIS, CONTENTS_GRATE, CONTENTS_AUX )
 	local trace = util.TraceLine( tr )
 	if (!trace.Hit) then return end
 
 	self:ShootEffects(trace.HitPos, trace.HitNormal, trace.Entity, trace.PhysicsBone, IsFirstTimePredicted()) 
+
+	if self.Second != false then 
+		net.Start("EndPosition")
+		net.WriteVector(self.Second)
+		net.WriteVector(trace.HitPos)
+		net.SendToServer()
+		self.Second = false
+		self.Owner:ChatPrint("End Coordinates Set!")
+	else 
+		self.Second = trace.HitPos 
+	end 
+end
+
+function SWEP:Reload()
+	if self.NextRunR and self.NextRunR >= CurTime() then return end 
+	self.NextRunR = CurTime()+0.1 
+	
+	local tr = util.GetPlayerTrace( self.Owner )
+	tr.mask = bit.bor( CONTENTS_SOLID, CONTENTS_MOVEABLE, CONTENTS_MONSTER, CONTENTS_WINDOW, CONTENTS_DEBRIS, CONTENTS_GRATE, CONTENTS_AUX )
+	local trace = util.TraceLine( tr )
+	if (!trace.Hit) then return end
+
+	self:ShootEffects(trace.HitPos, trace.HitNormal, trace.Entity, trace.PhysicsBone, IsFirstTimePredicted()) 
+
+	if self.RC != false then 
+		self.Owner:ChatPrint("GAMEMODE:AddACArea(Vector("..math.Round(math.min(self.First.x, trace.HitPos.x))..","..math.Round(math.min(self.First.y, trace.HitPos.y))..","..math.Round(math.min(self.First.z, trace.HitPos.z)).."),Vector("..math.Round(math.max(self.First.x, trace.HitPos.x))..","..math.Round(math.max(self.First.y, trace.HitPos.y))..","..math.Round(math.max(self.First.z, trace.HitPos.z))..")") 
+		self.RC = false
+	else 
+		self.RC = trace.HitPos 
+	end 
 end
 
 function SWEP:ShootEffects(hitpos, hitnormal, entity, physbone, bFirstTimePredicted)
--- ssbase/entities/weapons/ss_mapeditor/init.lua
+++ ssbase/entities/weapons/ss_mapeditor/init.lua
@@ -1,3 +1,5 @@
+util.AddNetworkString("StartPosition")
+util.AddNetworkString("EndPosition")
 
 AddCSLuaFile( "cl_init.lua" )
 AddCSLuaFile( "shared.lua" )
@@ -7,3 +9,66 @@ include( "shared.lua" )
 SWEP.Weight       = 5
 SWEP.AutoSwitchTo    = false
 SWEP.AutoSwitchFrom     = false
+
+net.Recieve("StartPosition",function(len,ply)
+	local v1,v2 = Vector(0,0,0),Vector(0,0,0)
+	local ov1,ov2 = net.ReadVector(),net.ReadVector()
+	v1.x = math.min(ov1.x,ov2.x)
+	v1.y = math.min(ov1.y,ov2.y)
+	v1.z = math.min(ov1.z,ov2.z)
+	v2.x = math.max(ov1.x,ov2.x)
+	v2.y = math.max(ov1.y,ov2.y)
+	v2.z = v1.z
+	ply.SZ = {v1,v2}
+end)
+
+net.Recieve("EndPosition",function(len,ply)
+	local v1,v2 = Vector(0,0,0),Vector(0,0,0)
+	local ov1,ov2 = net.ReadVector(),net.ReadVector()
+	v1.x = math.Round(math.min(ov1.x,ov2.x))
+	v1.y = math.Round(math.min(ov1.y,ov2.y))
+	v1.z = math.Round(math.min(ov1.z,ov2.z))
+	v2.x = math.Round(math.max(ov1.x,ov2.x))
+	v2.y = math.Round(math.max(ov1.y,ov2.y))
+	v2.z = v1.z
+	ply.EZ = {v1,v2}
+end)
+
+hook.Add("PlayerSay","addmap",function(ply,text,pub)
+	if(ply:HasWeapon("ss_mapeditor")) then
+		if(string.lower(text) == "!setspawn") then
+			local a = ply:GetAngles()
+			local p = ply:GetPos()
+			ply.StP = {Vector(math.Round(o.x),math.Round(o.y),math.Round(o.z)),Angle(math.Round(a.p),math.Round(a.y),math.Round(a.r))}
+			return
+		elseif(string.sub(string.lower(text),1,8) == "!addmap ") then
+			local args = string.Explode(" ",text)
+			table.remove(args,1)
+			if(!args[1] || tonumber(args[1]) == 0) then
+				ply:ChatPrint("Invalid Arguments!")
+				return
+			end
+			if(args[2]) then
+				ply:ChatPrint("Wrong number of arguments!")
+				return
+			end
+			if(!ply.SZ || !ply.EZ) then
+				ply:ChatPrint("No defined Start or End Zones")
+				return
+			end
+			if(!ply.StP) then
+				ply:ChatPrint("No defined Spawn Point!")
+				return
+			end
+			SS:AddMap(game.GetMap(),args[1],ply.StP[1],ply.StP[2],ply.SZ[1],ply.SZ[2],ply.EZ[1],ply.EZ[2])
+			ply.SZ = nil
+			ply.EZ = nil
+			ply.StP = nil
+			GAMEMODE:AreaSetup()
+			return
+		elseif(string.lower(text) == "!addmap") then
+			ply:ChatPrint("Invalid Arguments!")
+			return
+		end
+	end
+end)
