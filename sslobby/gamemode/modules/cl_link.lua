93191b7e2ddecb20213b594a9e77c7e6308c2515
Chewgum <chewgumtj@gmail.com>
2014-03-18 22:10:15 +0100

    is this real life

--- sslobby/gamemode/modules/cl_link.lua
+++ sslobby/gamemode/modules/cl_link.lua
@@ -54,9 +54,9 @@ end
 net.Receive("ss.lbgtsrs", function(bits)
 	local server = net.ReadUInt(8)
 	
-	storedTriggers[id].map = nil
-	storedTriggers[id].players = {}
-	storedTriggers[id].minimap = {}
+	storedTriggers[server].map = nil
+	storedTriggers[server].players = {}
+	storedTriggers[server].minimap = {}
 end)
 
 ---------------------------------------------------------
