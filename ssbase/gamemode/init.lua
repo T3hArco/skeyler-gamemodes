7bc9049e243a32c5e7d26863f470aafa425430dc
Chewgum <chewgumtj@gmail.com>
2014-03-19 18:46:41 +0100

    Fixed fonts

--- ssbase/gamemode/init.lua
+++ ssbase/gamemode/init.lua
@@ -17,7 +17,7 @@ end
 
 resource.AddWorkshop("239292201")
 
-resource.AddFile("resource/fonts/Arvil_Sans_0.ttf") 
+resource.AddFile("resource/fonts/arvilsans0.ttf") 
 
 resource.AddFile("resource/fonts/helveticaltstdboldcond.ttf") 
 resource.AddFile("resource/fonts/helveticaltstdlight.ttf") 
