8eec6db146b20814e8384baccb60ed022e2ebc4a
GeorgeRiley <george_ri@hotmail.co.uk>
2014-03-30 00:54:26 +0000

    anim stuff
    hi

--- ssbase/gamemode/shared.lua
+++ ssbase/gamemode/shared.lua
@@ -34,3 +34,64 @@ if (atlaschat) then
 	atlaschat.enableAvatars = atlaschat.config.New("Enable avatars?", "avatars", false, true, true, true, true)
 	atlaschat.enableRankIcons = atlaschat.config.New("Enable rank icons?", "rank_icons", false, true, true, true, true)
 end
+
+function GM:CalcMainActivity( ply, velocity )	
+
+	ply.CalcIdeal = ACT_MP_STAND_IDLE
+	ply.CalcSeqOverride = -1
+
+	if ( self:HandlePlayerNoClipping( ply, velocity ) ||
+		self:HandlePlayerDriving( ply ) ||
+		self:HandlePlayerVaulting( ply, velocity ) ||
+		self:HandlePlayerDucking( ply, velocity ) ||
+		self:HandlePlayerSwimming( ply, velocity ) ) then
+
+	else
+
+		local len2d = velocity:Length2D()
+		if ( len2d > 150 ) then ply.CalcIdeal = ACT_MP_RUN elseif ( len2d > 0.5 ) then ply.CalcIdeal = ACT_MP_WALK end
+
+	end
+
+	ply.m_bWasOnGround = ply:IsOnGround()
+	ply.m_bWasNoclipping = ( ply:GetMoveType() == MOVETYPE_NOCLIP && !ply:InVehicle() )
+
+	return ply.CalcIdeal, ply.CalcSeqOverride
+
+end
+
+function GM:DoAnimationEvent( ply, event, data )
+	if event == PLAYERANIMEVENT_ATTACK_PRIMARY then
+	
+		if ply:Crouching() then
+			ply:AnimRestartGesture( GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_MP_ATTACK_CROUCH_PRIMARYFIRE, true )
+		else
+			ply:AnimRestartGesture( GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_MP_ATTACK_STAND_PRIMARYFIRE, true )
+		end
+		
+		return ACT_VM_PRIMARYATTACK
+	
+	elseif event == PLAYERANIMEVENT_ATTACK_SECONDARY then
+	
+		-- there is no gesture, so just fire off the VM event
+		return ACT_VM_SECONDARYATTACK
+		
+	elseif event == PLAYERANIMEVENT_RELOAD then
+	
+		if ply:Crouching() then
+			ply:AnimRestartGesture( GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_MP_RELOAD_CROUCH, true )
+		else
+			ply:AnimRestartGesture( GESTURE_SLOT_ATTACK_AND_RELOAD, ACT_MP_RELOAD_STAND, true )
+		end
+		
+		return ACT_INVALID
+		
+	elseif event == PLAYERANIMEVENT_CANCEL_RELOAD then
+	
+		ply:AnimResetGestureSlot( GESTURE_SLOT_ATTACK_AND_RELOAD )
+		
+		return ACT_INVALID
+	end
+
+	return nil
+end
