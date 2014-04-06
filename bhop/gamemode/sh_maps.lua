7f4e21b9d6b6ce478ddb885870afaecc4be365c3
Arcky <ruben231994@gmail.com>
2014-04-06 18:29:00 +0200

    test

--- bhop/gamemode/sh_maps.lua
+++ bhop/gamemode/sh_maps.lua
@@ -10,6 +10,6 @@ function SS:AddMap(name, payout, spawnpos, spawnang, spawnareamin, spawnareamax,
 	SS:SaveMaps() 
 end 
 
-SS:AddMap("bhop_cobblestone_gm", 20000, Vector(68, 433, 192.88), Angle(0, -180, 0),
+--[[SS:AddMap("bhop_cobblestone_gm", 20000, Vector(68, 433, 192.88), Angle(0, -180, 0),
  Vector(-192, 194, 129), Vector(285, 670, 130),
- Vector(133, 2724, 129), Vector(360, 2870, 130))
+ Vector(133, 2724, 129), Vector(360, 2870, 130))]]
