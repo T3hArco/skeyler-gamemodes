6f413c456c9cfeddf6668e9a05874bd368bf9a42
GeorgeRiley <george_ri@hotmail.co.uk>
2014-04-13 16:04:51 +0100

    bhop_beautiful level select

---
+++ bhop/gamemode/mapfixes/bhop_beautiful.lua
@@ -0,0 +1,8 @@
+---------------------------- 
+--        Bunny Hop       -- 
+-- Created by Skeyler.com -- 
+---------------------------- 
+
+HOOKS["InitPostEntity"] = function()
+	GAMEMODE:AddACArea(Vector(-683, -1367, -63),Vector(409, -424, 51),"You have entered the level select area as a result of this your timer has been stopped.")
+end
