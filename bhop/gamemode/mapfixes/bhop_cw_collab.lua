8c03cc6f17906c75df987202bbe1c25548680690
GeorgeRiley <george_ri@hotmail.co.uk>
2014-05-02 19:59:44 +0100

    fix fix

--- bhop/gamemode/mapfixes/bhop_cw_collab.lua
+++ bhop/gamemode/mapfixes/bhop_cw_collab.lua
@@ -1,1 +1,37 @@
-resource.AddFile("scripts/soundscapes_bhop_cw_collab.txt") --it isnt pak'd right
+AddCSLuaFile()
+
+sound.Add( {
+	name = "s1",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 85,
+	pitch = 100,
+	sound = "aids/col1.mp3"
+} )
+
+sound.Add( {
+	name = "s2",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 85,
+	pitch = 100,
+	sound = "aids/col2.mp3"
+} )
+
+sound.Add( {
+	name = "s3",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 85,
+	pitch = 100,
+	sound = "aids/col3.mp3"
+} )
+
+sound.Add( {
+	name = "s4",
+	channel = CHAN_STATIC,
+	volume = 1.0,
+	level = 85,
+	pitch = 100,
+	sound = "aids/col4.mp3"
+} )
